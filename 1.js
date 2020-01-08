0090e3c58d0d0106aecc2cd5053c73084fb3fca6
nddasni <51874992+nddasni@users.noreply.github.com>
2020-01-08 03:02:45 -0400

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+let isRecaptchaFrame = () => {
+  return /https:\/\/www.google.com\/recaptcha\/api2\/anchor/.test(window.location.href);
+};
+
+let captchaInterval = setInterval(() => {
+  if (isRecaptchaFrame()) {
+    clearInterval(captchaInterval);
+    document.getElementsByClassName('recaptcha-checkbox-checkmark')[0].click();
+  }  
+}, 500);
